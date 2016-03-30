6672558667912e13564f2bf928dc1c9163f2286a
anamariasosam <anamariasosam@gmail.com>
2016-03-29 20:24:22 -0500

    add quantity to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328192255) do
+ActiveRecord::Schema.define(version: 20160330011717) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160328192255) do
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
     t.integer  "supplier_id"
+    t.integer  "quantity"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id"
