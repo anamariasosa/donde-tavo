350a059188aaa5030c6322d17ca7100643ebe2a2
anamariasosa <anamariasosam@gmail.com>
2016-05-25 22:11:55 -0500

    validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   validates_presence_of :name, :price, :quantity, :image
   validates_numericality_of :price
   validates :quantity, numericality: { only_integer: true, greater_than: 0 }
-  validates :price, numericality: { only_integer: true, greater_than: 1000 }
+  validates :price, length: { minimum: 4 }
   validates :name, uniqueness:true
   belongs_to :category
   belongs_to :supplier
